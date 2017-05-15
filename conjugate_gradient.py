7827d40d34f1b5734900c402ae7afd649403d93e
cgel <carlesgelada@hotmail.com>
2017-05-15 16:03:47 +0100

    moved conjugate_gradient to its own file and updated to use vectorify

---
+++ conjugate_gradient.py
@@ -0,0 +1,32 @@
+from vectorify import Vectorify
+
+
+def conjugate_gradient(Ax_f, b, iters, vervose=False):
+    #x = lset( lcopy(b), 0.)
+    x = b.copy() * 0
+    xx = Ax_f(x)
+    r = b - xx
+    d = r
+    if vervose:
+        print("Error:", (Ax_f(x) - b).norm())
+    for i in range(iters):
+        Ad = Ax_f(d)
+        Ad_scaling = Ad.norm() / d.norm()
+        dad = d.dot(Ad)
+        assert dad >= 0., "d^tAd=%.5f, so A is not possitive definite." % dad
+        alpha = r.dot(r) / dad
+        x = x + d * alpha
+        r_new = r - Ad * alpha
+        beta = r_new.dot(r_new) / r.dot(r)
+        d = r_new + d * beta
+        r = r_new
+        if vervose:
+            print("- iter:", i)
+            print("Error:", (Ax_f(x) - b).norm())
+        if vervose > 1:
+            print("dad:", dad)
+            print("alpha:", alpha)
+            print("beta:", beta)
+            print("Ad scaling:", Ad_scaling)
+            print("d norm:", d.norm())
+    return x
