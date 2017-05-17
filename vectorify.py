1a4684c71129473c809aa9b4ffe6c3565f724af7
cgel <carlesgelada@hotmail.com>
2017-05-17 14:47:42 +0000

    solve bugs in vectorify

--- vectorify.py
+++ vectorify.py
@@ -1,20 +1,26 @@
 import numpy as np
+import pickle
 
 
 class Vectorify:
 
     def __init__(self, vec):
-        if not isinstance(vec, list):
-            vec = [vec]
-        self.data = vec
-        self.size = sum([v.size for v in vec])
+        if isinstance(vec, Vectorify):
+            self.data = [x.copy() for x in vec.data]
+        elif isinstance(vec, list):
+            self.data = vec
+        elif isinstance(vec, str):
+            self.data = pickle.load(vec)
+        elif not isinstance(vec, list):
+            self.data = [vec]
+        self.size = sum([v.size for v in self.data])
 
     def compatible(self, vec):
         assert isinstance(vec, Vectorify)
         if len(self.data) != len(vec.data):
             return False
         for v1, v2 in zip(self.data, vec.data):
-            if v1.size) != v2.size:
+            if v1.size != v2.size:
                 return False
         return True
 
@@ -26,11 +32,14 @@ class Vectorify:
             res=list(map(lambda x: np.add(x, a), self.data))
         # elementwise product
         elif isinstance(a, Vectorify):
-            assert self.compatible(vec.size)
-            res = Vectorify(list(map(np.add, self.data, a.data)))
+            assert self.compatible(a)
+            res = list(map(np.add, self.data, a.data))
         else:
             Exception("Cannot add Vectorify and %s" % (type(a)))
-        return res
+        return Vectorify(res)
+
+    def __radd__(self, a):
+        return self + a
 
     def __mul__(self, a):
         if isinstance(a, list):
@@ -40,13 +49,13 @@ class Vectorify:
             res = list(map(lambda x: np.multiply(x, a), self.data))
         # elementwise product
         elif isinstance(a, Vectorify):
-            assert self.compatible(vec.size)
-            res=Vectorify(list(map(np.multiply, self.data, a.data)))
+            assert self.compatible(a)
+            res=list(map(np.multiply, self.data, a.data))
         else:
             Exception("Cannot multiply Vectorify and %s" % (type(a)))
-        return res
+        return Vectorify(res)
 
-    def __sub__(self, vec):
+    def __sub__(self, a):
         if isinstance(a, list):
             a=Vectorify(a)
         # scalar product
@@ -54,13 +63,13 @@ class Vectorify:
             res=list(map(lambda x: np.subtract(x, a), self.data))
         # elementwise product
         elif isinstance(a, Vectorify):
-            assert self.compatible(vec.size)
-            res=Vectorify(list(map(np.subtract, self.data, a.data)))
+            assert self.compatible(a)
+            res=list(map(np.subtract, self.data, a.data))
         else:
             Exception("Cannot subtract Vectorify and %s" % (type(a)))
-        return res
+        return Vectorify(res)
 
-    def __truediv__(self, vec):
+    def __truediv__(self, a):
         if isinstance(a, list):
             a=Vectorify(a)
         # scalar product
@@ -68,34 +77,41 @@ class Vectorify:
             res=list(map(lambda x: np.divide(x, a), self.data))
         # elementwise product
         elif isinstance(a, Vectorify):
-            assert self.compatible(vec.size)
-            res=Vectorify(list(map(np.divide, self.data, a.data)))
+            assert self.compatible(a)
+            res=list(map(np.divide, self.data, a.data))
         else:
             Exception("Cannot divide Vectorify and %s" % (type(a)))
-        return res
+        return Vectorify(res)
 
     def dot(self, a):
         if isinstance(a, list):
             a=Vectorify(a)
-        assert self.compatible(vec.size)
+        assert self.compatible(a)
         prod=self * a
-        return sum(list(map(np.sum, prods)))
+        return sum(list(map(np.sum, prod)))
 
     def norm(self):
-        return sum(list(map(np.sum, lprod(self, self))))**0.5
+        return sum(list(map(np.sum, (self*self).data)))**0.5
 
     def assign(self, a):
         if isinstance(a, list):
             a=Vectorify(a)
         if isinstance(a, int) or isinstance(a, float):
-            res=Vectorify(list(map(lambda x: x[:]=a, self.data)))
+            for x in self.data:
+                x[:] = a
         # elementwise product
         elif isinstance(a, Vectorify):
-            assert self.compatible(vec.size)
-            res=Vectorify(list(map(lambda x, y: x[:]=y, self.data, a.data)))
+            assert self.compatible(a)
+            for x,y in zip(self.data, a.data):
+                x[:] = y
         else:
-            Exception("Cannot divide Vectorify and %s" % (type(a)))
-        return res
+            Exception("Cannot assign %s to Vectorify" % (type(a)))
 
     def copy(self):
         return Vectorify([x.copy() for x in self.data])
+
+    def __getitem__(self, i):
+        return self.data[i]
+
+    def save(self, filename):
+        pickle.dump(self.data, open(filename, "wb") )
