3add7d58068bc1a7c1e762fee910d4c87cdcfa18
cgel <carlesgelada@hotmail.com>
2017-06-01 00:19:19 +0100

    better name scoping

--- Hv_prod.py
+++ Hv_prod.py
@@ -3,19 +3,18 @@ import numpy as np
 from vectorify import Vectorify
 
 
-def Hv_prod(x, grads=None):
-    if grads == None:
-        grads = tf.gradients(loss, tf.global_variables(),
-                             name="grads_for_influence")
-    grads = [g for g in grads if g is not None]
-
+def Hv_prod(x, grads=None, name="Hessian_vector_product"):
     # extend the graph to compute Hv products
-    with tf.name_scope("Hv_prod"):
+    with tf.name_scope(name):
+        if grads == None:
+            grads = tf.gradients(x, tf.trainable_variables(),
+                             name="grads")
+        grads = [g for g in grads if g is not None]
         # vector placeholder (a vector)
         with tf.name_scope("v"):
             vecs = [tf.placeholder(tf.float32, grads[i].get_shape(), name="v" + str(i)+"_ph") for i in range(len(grads))]
         # gradient vector product (a scalar)
-        with tf.name_scope("gvp"):
+        with tf.name_scope("grads_v_product"):
             tt = 1
             gvp = [tf.reduce_sum(grads[i] * vecs[i], name="gvp"+str(i) ) for i in range(len(grads))]
         # Hessian vector product (a vector)
